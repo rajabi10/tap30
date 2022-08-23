3c1f04b83bf250b472fa99c1b628d80e5b8b4664
Negin Rajabi <negin171378@gmail.com>
2022-08-23 11:50:05 +0430

    uncomment toggler code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// const toggler = document.querySelector(".nav__toggler");
-// const navbar = document.querySelector(".nav");
-// toggler.addEventListener("click", (e) => {
-//   navbar.classList.toggle("nav__expanded");
-// });
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
 
 const tabs = document.querySelectorAll(".tab");
 const tabContents = document.querySelectorAll(".tab-content");
