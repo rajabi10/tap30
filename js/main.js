1c96752e3f83d090f58c2e858d49df16b4e0dd4f
Negin Rajabi <negin171378@gmail.com>
2022-08-30 14:41:56 +0430

    change the line of accordion function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+const accordion = document.querySelector(".accordion");
+accordionHeaders.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.parentElement);
+    e.target.parentElement.classList.toggle("accordion__expanded");
+  });
+});
+
 const toggler = document.querySelector(".nav__toggler");
 const navbar = document.querySelector(".nav");
 toggler.addEventListener("click", (e) => {
@@ -17,14 +26,5 @@ tabs.forEach((tab) => {
     // ADD NEW ACTIVE CLASSES
     tab.classList.add("active");
     targeTabContent.classList.add("active");
-  });
-});
-
-const accordionHeaders = document.querySelectorAll(".accordion__header");
-const accordion = document.querySelector(".accordion");
-accordionHeaders.forEach((item) => {
-  item.addEventListener("click", (e) => {
-    console.log(e.target.parentElement);
-    e.target.parentElement.classList.toggle("accordion__expanded");
   });
 });
